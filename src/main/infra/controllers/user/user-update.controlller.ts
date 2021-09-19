f0d1169b212c04cd65c3b307b566fc1dd7cce4f7
duraes-antonio <garotoseis@gmail.com>
2021-09-19 05:09:11 -0300

    NEW: Base controller; REFACT: Isolate user controller by use case

---
+++ src/main/infra/controllers/user/user-update.controlller.ts
@@ -0,0 +1,25 @@
+import { BaseController, ControllerSuccessResponse } from '../base.controller';
+import {
+    IUpdateUserCase,
+    UpdateUserInput,
+} from '../../../../core/modules/user/core/use-cases/update-user';
+import { StatusCodes } from 'http-status-codes';
+
+export class UpdateUserController extends BaseController<
+    UpdateUserInput,
+    void
+> {
+    constructor(private readonly updateUseCase: IUpdateUserCase) {
+        super();
+    }
+
+    protected async handleRequest(
+        input: UpdateUserInput
+    ): Promise<ControllerSuccessResponse<void>> {
+        const result = await this.updateUseCase.execute(input);
+        return {
+            code: StatusCodes.OK,
+            result,
+        };
+    }
+}
