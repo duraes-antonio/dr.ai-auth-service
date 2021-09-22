38dc69a74ef69864a7a3b512fb8b62071a8f06c3
duraes-antonio <garotoseis@gmail.com>
2021-09-22 02:25:50 -0300

    NEW: Routes for user operations and server use

---
+++ src/main/server.ts
@@ -0,0 +1,10 @@
+import { USER_ROUTES } from './infra/routes/user.routes';
+import { getContainerDI } from './config/dependency-injection/inversify/containers/di-container';
+import { Server } from './infra/http/server';
+import { ADAPTERS_TYPES } from './config/dependency-injection/inversify/di-types';
+
+const containerDI = getContainerDI();
+const server = containerDI.get<Server>(ADAPTERS_TYPES.Server);
+const port = process.env.PORT ?? 3000;
+server.configureRoutes(USER_ROUTES);
+server.listen(port).then();
