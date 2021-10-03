2fdcb48136f68cae9a1bfb89366e2f0c31f21fc0
duraes-antonio <garotoseis@gmail.com>
2021-10-03 18:10:11 -0300

    FIX: Invalid directory for user register controller

--- src/main/infra/routes/user/user.routes.ts
+++ src/main/infra/routes/user/user.routes.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import { HttpMethods, HttpRouteInput } from '../../http/http.models';
 import { BaseController } from '../../controllers/base.controller';
 import { getContainerDI } from '../../../config/dependency-injection/inversify/containers/di-container';
-import { RegisterUserController } from '../../controllers/user/user-register.controller';
+import { RegisterUserController } from '../../controllers/user/register/user-register.controller';
 import { UpdateUserController } from '../../controllers/user/user-update.controlller';
 import { createRouteInput } from '../routes';
 
